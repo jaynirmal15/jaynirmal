8d206184762372944d6e44cd18f245f40fc6ec11
Nirmal Jay <nirmaljay@Nirmals-MacBook-Pro.local>
2020-01-23 15:41:31 -0500

    Exercise Tracker Added

---
+++ ExerciseTracker/src/components/edit-exercises.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class EditExercise extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>You are on Edit Exercise  Component</p>
+            </div>
+        )
+    }
+}
---
+++ ExerciseTracker/src/components/exercises-list.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class ExerciseList extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>You are on Exercise List Component</p>
+            </div>
+        )
+    }
+}
